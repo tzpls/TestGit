33c4276b792e60093e9ee4502d7b5f367e543158
tzp <tzpls@126.com>
2020-05-21 10:36:11 +0800

    my test

---
+++ src/main/java/com/attzp/git/Apple.java
@@ -0,0 +1,8 @@
+package com.attzp.git;
+
+public class Apple {
+	public static void main(String[] args) {
+		System.out.println("apple---------------");
+	}
+
+}
---
+++ src/test/java/com/atzp/test/GoodTest.java
@@ -0,0 +1,5 @@
+package com.atzp.test;
+
+public class GoodTest {
+
+}
