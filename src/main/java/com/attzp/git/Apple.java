cb8d15f3926b8701a90d5ab42a00010b24f3a1fd
tzp <tzpls@126.com>
2020-05-21 11:05:11 +0800

    ee

--- src/main/java/com/attzp/git/Apple.java
+++ src/main/java/com/attzp/git/Apple.java
@@ -2,7 +2,8 @@ package com.attzp.git;
 
 public class Apple {
 	public static void main(String[] args) {
-		System.out.println("apple---------------");
+		System.out.println("apple-----5555555555----4444------"); 
+		System.out.println("000000000000000000000000000000");
 	}
 
 }
